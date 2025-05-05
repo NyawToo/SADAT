05f16242baa7e8a563815de21b2985e8ac2027a0
NyawToo <julianandresperez904@hotmail.com>
2025-05-05 09:28:33 -0500

    Build Fix Admin

--- build.sh
+++ build.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
-
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
+python -m pip install --upgrade pip
+
+# Crear superusuario automáticamente
+echo "Creando superusuario..."
+DJANGO_SUPERUSER_USERNAME=Kevin \
+DJANGO_SUPERUSER_EMAIL=kevxn_garcxa@outlook.com \
+DJANGO_SUPERUSER_PASSWORD=julianpan123 \
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='Kevin').exists():
+    User.objects.create_superuser(
+        username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
+        email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+        password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    );
+    print('Superusuario creado exitosamente.');
+else:
+    print('El superusuario ya existe.');
+"
